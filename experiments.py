a80b85b0c138078949b5820a113186c5d320d075
krabay-hifly <krabay-hifly>
2024-02-17 19:05:22 +0000

    initial prompts set, now turning to class init

--- experiments.py
+++ experiments.py
@@ -38,6 +38,191 @@ print(response)
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC #### Set Prompts
+# MAGIC
+# MAGIC The following players will be included
+# MAGIC 1. Master: Responsible for assigning 2 debators with their arguments. By the end, Master will be given Moderator's assessments and pick a final winner.
+# MAGIC 2. Moderator: Based on topic sets talking points, for each talking point facilitates an {n} round discussion. [Later purpose: ask followup questions]. After each talking point is finished, Moderator will make an objective judgement of which debater was more convincing for that part of the debate.
+# MAGIC 3. Debaters: Given their arguments and a topic, these two AI agents will exchange ideas, argue with one another.
+
+# COMMAND ----------
+
+master_prompt_system_message = "You are part of an AI simulation. In this world different AIs debate one another. You are the master of this world."
+
+master_prompt_instruction = """
+Your tasks are the following:
+
+1. Given a debate topic you will assign 2 debaters with their sides, what they need to argue for. These are not necessary just affirmative or negative. There may be cases where one of them needs to argue for or compare certain products, services, TV-shows, books, political ideas, theories, etc... In these cases you will assign each debater with one of the two options.
+
+2. After the debate is finished, you'll be given a complete transcript of the discussion. Your task will be to summarize what each debater talked about, what their arguments were, overall how you evaluate the debate. Then, you will pick a winner based on whose arguments were more objective, thorough, factual and convincing.
+
+Here's the topic of today's debate: {topic}
+
+First, describe what debater #1 will be arguing for. You're directly talking to debater #1, describe to them the topic and the side they will be taking. Do not help him by listing talking points or pro-contra arguments, simply articulate his task.
+
+Debater #1's side:
+"""
+
+master_prompt_instruction_second_debater = """
+
+Secondly, describe what debater #2 will be arguing for. Make sure debater #1 and debater #2 are arguing for the opposite sides. You're directly talking to debater #2, describe to them the topic and the side they will be taking. Do not help him by listing talking points or pro-contra arguments, simply articulate his task.
+
+Debater #2's side:
+"""
+
+# COMMAND ----------
+
+moderator_system_message = "You are part of an AI simulation. In this world different AIs debate one another. You are the moderator of the debates."
+
+moderator_prompt_instruction = """
+Here's the topic of the debate: {topic}
+
+Here's the instruction debater #1 received: {debater_1_instruction}
+And here's what debater #2 received as instruction: {debater_2_instruction}
+
+You are an expert in this field. You're given two tasks:
+
+1. First, using your deep understanding of the topics, come up with {n_talking_points} talking points or aspects the debaters should cover. These aspects will serve as the agenda, the different angles the debaters should be looking at when making their arguments.
+
+2. Secondy, you will allow the debaters to discuss each aspect for {n_rounds} rounds. Once they reach the final round, you'll make a judgement of which debater presented more convincing arguments and pick a winner. You'll shortly summarize their reasoning to help explain your decision when picking a winner. Then you'll move on to the next aspect.
+
+So, given the topic, come up with the {n_talking_points} talking points / aspects. Only return with the aspects, separated by a semicolon.
+
+Debate talking points:
+"""
+
+# COMMAND ----------
+
+debater_1_system_message = "You are part of an AI simulation. In this world different AIs debate one another. You are debater #1."
+
+debater_1_prompt_instruction = """
+Here's the topic of the debate: {topic}
+
+Here's your instruction: {debater_1_instruction}
+
+The moderator of this debate had come up with {n_talking_points} talking points / aspects. 
+The current aspect of this topic that you are arguing for is {current_talking_point}.
+
+Focus on being concise, as too long answers will lose the attention of the audience and the moderator. Make your arguments, reason, be to-the-point.
+
+As debater #1, you will start the discussion.
+"""
+
+# COMMAND ----------
+
+debater_2_system_message = "You are part of an AI simulation. In this world different AIs debate one another. You are debater #2."
+
+debater_2_prompt_instruction = """
+Here's the topic of the debate: {topic}
+
+Here's your instruction: {debater_2_instruction}
+
+The moderator of this debate had come up with {n_talking_points} talking points / aspects. 
+The current aspect of this topic that you are arguing for is {current_talking_point}.
+
+Focus on being concise, as too long answers will lose the attention of the audience and the moderator. Make your arguments, reason, be to-the-point.
+
+Debater #1 had started the discussion, as debater #2, you are to respond and make your own arguments.
+"""
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Set Agent and Debate classes
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+topic = "Which one is the better social media platform? Facebook or Instagram?"
+n_talking_points = 5
+n_rounds = 10
+
+messages = [{'role' : 'system', 'content' : master_prompt_system_message},
+            {'role' : 'user', 'content' : master_prompt_instruction.format(topic = topic)}]
+
+debater_1_instruction, usage = generate_response(messages)
+
+# COMMAND ----------
+
+messages = [{'role' : 'system', 'content' : master_prompt_system_message},
+            {'role' : 'user', 'content' : master_prompt_instruction.format(topic = topic)},
+            {'role' : 'assistant', 'content' : debater_1_instruction},
+            {'role' : 'user', 'content' : master_prompt_instruction_second_debater}]
+            
+debater_2_instruction, usage = generate_response(messages)
+
+# COMMAND ----------
+
+messages = [{'role' : 'system', 'content' : moderator_system_message},
+            {'role' : 'user', 'content' : moderator_prompt_instruction.format(topic = topic, 
+                                                                              debater_1_instruction = debater_1_instruction,
+                                                                              debater_2_instruction = debater_2_instruction,
+                                                                              n_talking_points = n_talking_points, 
+                                                                              n_rounds = n_rounds)}]
+            
+moderator_talking_points, usage = generate_response(messages)
+
+# COMMAND ----------
+
+moderator_talking_points_list = moderator_talking_points.split(';')
+current_talking_point = moderator_talking_points_list[0]
+current_talking_point
+
+# COMMAND ----------
+
+messages = [{'role' : 'system', 'content' : debater_1_system_message},
+            {'role' : 'user', 'content' : debater_1_prompt_instruction.format(topic = topic, 
+                                                                              debater_1_instruction = debater_1_instruction,
+                                                                              n_talking_points = n_talking_points, 
+                                                                              current_talking_point = current_talking_point)}]
+            
+debater_1_first_argument, usage = generate_response(messages)
+
+# COMMAND ----------
+
+debater_1_first_argument
+
+# COMMAND ----------
+
+messages = [{'role' : 'system', 'content' : debater_2_system_message},
+            {'role' : 'user', 'content' : debater_2_prompt_instruction.format(topic = topic, 
+                                                                              debater_2_instruction = debater_2_instruction,
+                                                                              n_talking_points = n_talking_points, 
+                                                                              current_talking_point = current_talking_point)},
+            {'role' : 'assistant', 'content' : debater_1_first_argument},]
+            
+debater_2_first_argument, usage = generate_response(messages)
+
+# COMMAND ----------
+
+debater_2_first_argument
+
+# COMMAND ----------
+
+messages = [{'role' : 'system', 'content' : debater_1_system_message},
+            {'role' : 'user', 'content' : debater_1_prompt_instruction.format(topic = topic, 
+                                                                              debater_1_instruction = debater_1_instruction,
+                                                                              n_talking_points = n_talking_points, 
+                                                                              current_talking_point = current_talking_point)},
+            {'role' : 'assistant', 'content' : debater_1_first_argument},
+            {'role' : 'assistant', 'content' : debater_2_first_argument},]
+            
+debater_1_second_argument, usage = generate_response(messages)
+
+# COMMAND ----------
+
+debater_1_second_argument
+
+# COMMAND ----------
+
 
 
 # COMMAND ----------
